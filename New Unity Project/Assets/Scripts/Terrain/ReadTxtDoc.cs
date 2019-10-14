9d3c08b6fb37a7f638b607b93053d84e6a988fba
nickstu14 <nickstu14@hotmail.co.uk>
2019-10-14 20:56:03 +0100

    Animation & level creation

--- New Unity Project/Assets/Scripts/Terrain/ReadTxtDoc.cs
+++ New Unity Project/Assets/Scripts/Terrain/ReadTxtDoc.cs
@@ -9,12 +9,13 @@ public class ReadTxtDoc : MonoBehaviour
     public string LevelName;
     public string m_FileLoc;
     public string m_ApplicationPath;
-    public List<string> m_Level;
-    public string m_Line;
+    private List<string> m_Level;
+    private string m_Line;
     public CityObjects[] m_City;
     public List<CityMap> m_Map;
-    public CityMap m_TempMap;
+    private CityMap m_TempMap;
     private int m_Count;
+    public float m_Offset;
 
 
     // Start is called before the first frame update
@@ -23,7 +24,7 @@ public class ReadTxtDoc : MonoBehaviour
         m_ApplicationPath = Application.dataPath;
 
         if (m_FileLoc != null)
-            m_File = new StreamReader(@"" + m_ApplicationPath + "\\LevelTxt\\" + LevelName +".txt");
+            m_File = new StreamReader(@"" + m_ApplicationPath + "\\LevelTxt\\" + LevelName + ".txt");
 
 
         m_Level = new List<string>();
@@ -35,6 +36,8 @@ public class ReadTxtDoc : MonoBehaviour
             m_Count++;
         }
         m_File.Close();
+
+        SetLevel();
     }
 
     // Update is called once per frame
@@ -45,55 +48,72 @@ public class ReadTxtDoc : MonoBehaviour
 
     void ReadMap(string _Line, int _Count)
     {
+        int m_J = 0;
+
         for (int m_I = 0; m_I < _Line.Length; m_I++)
         {
+            m_J = m_I;
             m_TempMap = new CityMap();
-            m_TempMap.m_Coor.x = m_I;
+            m_TempMap.m_Coor.x = m_J;
             m_TempMap.m_Coor.y = _Count;
             m_TempMap.m_Character = _Line[m_I];
 
-            if((_Line[m_I+1] == '{') && (_Line[m_I+3] == '}'))
-            { //Config from anouther file
-                m_I += 1;
-                m_TempMap = FindConfig(m_TempMap, _Line[m_I]);
-                m_I += 1;
-            }
-            else if(_Line[m_I+1] == '{' && _Line[m_I+3] != '}' )
-            { //internal config
-                CityConfig m_cityConfig = CityConfig.Rotation;
-                m_I ++;
-                do
-                {
+            m_J++;
 
-                    if (_Line[m_I] == '{' || _Line[m_I] == ' ')
-                    { // skip if any spaces or 
-                        m_I++;
-                    }
-                    else if (_Line[m_I] == ',')
+            if (m_I+1 < _Line.Length)
+            {
+                if ((_Line[m_I +1] == '{') && (_Line[m_I + 3] == '}'))
+                { //Config from anouther file
+                    m_I++;
+                    m_TempMap = FindConfig(m_TempMap, _Line[m_I + 1]);
+                    m_I++;
+                }
+                else if (_Line[m_I+1] == '{' && _Line[m_I + 3] != '}')
+                { //internal config
+                    CityConfig m_cityConfig = CityConfig.Rotation;
+                    m_I++;
+                    do
                     {
-                        m_cityConfig++;
-                        m_I++;
-                    }
-                    else
-                    {
-                        switch (m_cityConfig)
-                        {
-                            case CityConfig.Rotation: m_TempMap.m_Rotation = _Line[m_I];
-                                break;
-                            //case CityConfig.Scale: m_TempMap.m_Scale = _Line[m_I];
-                             //   break;
-                            case CityConfig.Colour:
-                                m_TempMap = GetColour(m_TempMap ,_Line, m_I);
-                                break;
+
+                        if (_Line[m_I] == '{' || _Line[m_I] == ' ')
+                        { // skip if any spaces or 
+                            m_I++;
                         }
-                        m_I++;
-                    }
+                        else if (_Line[m_I] == ',')
+                        {
+                            m_cityConfig++;
+                            m_I++;
+                        }
+                        else
+                        {
+                            switch (m_cityConfig)
+                            {
+                                case CityConfig.Rotation:
+                                    m_TempMap.m_Rotation = _Line[m_I];
+                                    break;
+                                //case CityConfig.Scale: m_TempMap.m_Scale = _Line[m_I];
+                                //   break;
+                                case CityConfig.Colour:
+                                    m_TempMap = GetColour(m_TempMap, _Line, m_I);
+                                    break;
+                            }
+                            m_I++;
+                        }
 
-                } while (_Line[m_I] == '}');
+                    } while (_Line[m_I] != '}');
+                }
+                else
+                {
+                    //default config
+                    m_TempMap.m_Colour = Color.grey;
+                    m_TempMap.m_Rotation = Random.Range(0, 4);
+                }
             }
             else
             {
                 //default config
+                m_TempMap.m_Colour = Color.grey;
+                m_TempMap.m_Rotation = Random.Range(0, 4);
             }
             m_Map.Add(m_TempMap);
 
@@ -109,17 +129,79 @@ public class ReadTxtDoc : MonoBehaviour
 
     CityMap GetColour(CityMap _Data, string _Line, int _Value)
     {
+        //string m_Colour;
+        //string m_Value;
 
+        if (_Line[_Value + 1] == '[')
+        { //[255,255,255]
+            do
+            {
+
+                _Value++;
+            } while (_Line[_Value] != ']');
+        }
+        else
+        {//colour
+
+
+            do
+            {
+                // m_Value += _Line[_Value];
+
+                _Value++;
+            } while (_Line[_Value] != ',');
+
+        }
 
         return _Data;
     }
 
-    
-   /* Vector3 GetScale()
+
+    void SetLevel()
     {
+        foreach (CityMap m_MO in m_Map)
+        {
+            GameObject m_Level = Instantiate(GetObject(m_MO.m_Character), new Vector3(m_MO.m_Coor.x * m_Offset, 0, m_MO.m_Coor.y * m_Offset), GetRotation(m_MO.m_Rotation), transform.parent);
+        }
 
-    }*/
+    }
+    /* Vector3 GetScale()
+     {
 
+     }*/
+
+    GameObject GetObject(char _Character)
+    {
+        foreach (CityObjects m_CO in m_City)
+        {
+            if (m_CO.m_Character == _Character)
+            {
+                return m_CO.m_Object;
+            }
+        }
+        return new GameObject();
+    }
+    Quaternion GetRotation(int _Val)
+    {
+        Quaternion m_rot = new Quaternion();
+
+        switch (_Val)
+        {
+            case 0:
+                m_rot = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+                break;
+            case 1:
+                m_rot = new Quaternion(0.0f, 90f, 0.0f, 0.0f);
+                break;
+            case 2:
+                m_rot = new Quaternion(0.0f, 180f, 0.0f, 0.0f);
+                break;
+            case 3:
+                m_rot = new Quaternion(0.0f, 270f, 0.0f, 0.0f);
+                break;
+        }
+        return m_rot;
+    }
 
     [System.Serializable]
     public class CityObjects
@@ -134,14 +216,14 @@ public class ReadTxtDoc : MonoBehaviour
         public Vector2 m_Coor;
         [Space(2)]
         public char m_Character;
-        public char m_Rotation;
-        public char m_Scale;
-        public char m_Colour;
+        public int m_Rotation;
+        //public Vector3 m_Scale;
+        public Color m_Colour;
     }
     private enum CityConfig
     {
         Rotation = 1,
-       // Scale = 2,
+        // Scale = 2,
         Colour
     }
 
